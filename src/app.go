402734bb70c276dcea43b4ae47b64fb6727b773f
Ian Tracey <ianrtracey@gmail.com>
2016-07-15 18:11:52 -0700

    Adds voting items logic

--- src/app.go
+++ src/app.go
@@ -21,9 +21,14 @@ type SlackToken struct {
 	Token string `json:"api_token"`
 }
 
-type Trial struct {
+type Ballet struct {
 	Topic string
 	Items []string
+}
+
+func (ballet *Ballet) AddItem(item string) []string {
+	ballet.Items = append(ballet.Items, item)
+	return ballet.Items
 }
 
 func heartBeatHandler(w http.ResponseWriter, r *http.Request) {
@@ -41,7 +46,7 @@ func getToken() ([]byte, error) {
 	return ioutil.ReadFile("./token.json")
 }
 
-func HandleParliamentMessage(ev *slack.MessageEvent, rtm *slack.RTM, trial *fsm.FSM) {
+func HandleParliamentMessage(ev *slack.MessageEvent, rtm *slack.RTM, trial *fsm.FSM, ballet *Ballet) {
 	fmt.Println(trial.Current())
 	switch trial.Current() {
 	case "inactive":
@@ -64,9 +69,19 @@ func HandleParliamentMessage(ev *slack.MessageEvent, rtm *slack.RTM, trial *fsm.
 		rtm.SendMessage(rtm.NewOutgoingMessage("I didn't quite catch that", ev.Channel))
 
 	case "waiting_on_topic":
-		rtm.SendMessage(rtm.NewOutgoingMessage("Cool, tell me some of the voting options:", ev.Channel))
 		trial.Event("submit_topic")
+		rtm.SendMessage(rtm.NewOutgoingMessage("Cool, tell me some of the voting options:", ev.Channel))
 		return
+
+	case "waiting_on_items":
+		// need to add check to verify that the speaker of the house actually submitted at least two items
+		if strings.Contains(ev.Text, "done") {
+			trial.Event("complete_items")
+			rtm.SendMessage(rtm.NewOutgoingMessage("Awesome! Commence the voting! DM'ing the channel...", ev.Channel))
+			return
+		}
+		ballet.AddItem(ev.Text)
+		fmt.Println(ballet.Items)
 
 	default:
 		panic("unhandled case!")
@@ -82,9 +97,11 @@ func main() {
 			{Name: "confirm_trial", Src: []string{"awaiting_confirmation_of_trial"}, Dst: "waiting_on_topic"},
 			{Name: "decline_trial", Src: []string{"awaiting_confirmation_of_trial"}, Dst: "inactive"},
 			{Name: "submit_topic", Src: []string{"waiting_on_topic"}, Dst: "waiting_on_items"},
+			{Name: "complete_items", Src: []string{"waiting_on_items"}, Dst: "preparing_ballet"},
 		},
 		fsm.Callbacks{},
 	)
+	ballet := &Ballet{}
 
 	logger := log.New(os.Stdout, "slack-bot:", log.Lshortfile|log.LstdFlags)
 	// need to set bot_handle to be dynamic as the id could change depending on the instance
@@ -119,12 +136,11 @@ Loop:
 			case *slack.ConnectedEvent:
 				fmt.Println("Infos:", ev.Info)
 				fmt.Println("COnnection counter:", ev.ConnectionCount)
-				rtm.SendMessage(rtm.NewOutgoingMessage("Hello World", "#parliament-test"))
 
 			case *slack.MessageEvent:
 				fmt.Printf("Message: %v\n", ev)
 				if strings.Contains(ev.Text, bot_handle) {
-					HandleParliamentMessage(ev, rtm, trial)
+					HandleParliamentMessage(ev, rtm, trial, ballet)
 				}
 				fmt.Printf("Trial: %v\n", trial)
 
