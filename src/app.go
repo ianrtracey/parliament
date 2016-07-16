8fdf9da7fa13ffdad99660f1ec361e83da5d3d12
Ian Tracey <ianrtracey@gmail.com>
2016-07-15 17:30:39 -0700

    Adds finite state machine logic for handling trial confirmations

--- src/app.go
+++ src/app.go
@@ -3,15 +3,14 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"net/http"
 	"io/ioutil"
-	"os"
 	"log"
+	"net/http"
+	"os"
 	"strings"
 
-	"github.com/nlopes/slack"
 	"github.com/looplab/fsm"
-
+	"github.com/nlopes/slack"
 )
 
 type Message struct {
@@ -23,7 +22,8 @@ type SlackToken struct {
 }
 
 type Trial struct {
-	State *fsm.FSM
+	Topic string
+	Items []string
 }
 
 func heartBeatHandler(w http.ResponseWriter, r *http.Request) {
@@ -41,34 +41,58 @@ func getToken() ([]byte, error) {
 	return ioutil.ReadFile("./token.json")
 }
 
-func (trial *Trial) enterState(e *fsm.Event) {
-    fmt.Printf("The door to %s is %s\n", trial.State, e.Dst)
+func HandleParliamentMessage(ev *slack.MessageEvent, rtm *slack.RTM, trial *fsm.FSM) {
+	fmt.Println(trial.Current())
+	switch trial.Current() {
+	case "inactive":
+		rtm.SendMessage(rtm.NewOutgoingMessage("Here ye, here ye! Should we hold a trial?", ev.Channel))
+		trial.Event("start")
+
+	case "awaiting_confirmation_of_trial":
+		if strings.Contains(ev.Text, "yes") {
+			rtm.SendMessage(rtm.NewOutgoingMessage("Cool, let's get started :firework:", ev.Channel))
+			trial.Event("confirm_trial")
+			rtm.SendMessage(rtm.NewOutgoingMessage("What is the purpose this trial?", ev.Channel))
+			return
+		}
+
+		if strings.Contains(ev.Text, "no") {
+			rtm.SendMessage(rtm.NewOutgoingMessage("Alright, see you next time :cry:", ev.Channel))
+			trial.Event("decline_trial")
+			return
+		}
+		rtm.SendMessage(rtm.NewOutgoingMessage("I didn't quite catch that", ev.Channel))
+
+	case "waiting_on_topic":
+		rtm.SendMessage(rtm.NewOutgoingMessage("Cool, tell me some of the voting options:", ev.Channel))
+		trial.Event("submit_topic")
+		return
+
+	default:
+		panic("unhandled case!")
+	}
 }
-
-
 
 func main() {
 
-
-	trial := &Trial{}
-
-	trial.State = fsm.NewFSM(
+	trial := fsm.NewFSM(
 		"inactive",
 		fsm.Events{
-			{Name: "start", Src: []string{"inactive"}, Dst: "started"},
-			{Name: "submit_topic", Src: []string{"started"}, Dst: "waiting_on_items"},
+			{Name: "start", Src: []string{"inactive"}, Dst: "awaiting_confirmation_of_trial"},
+			{Name: "confirm_trial", Src: []string{"awaiting_confirmation_of_trial"}, Dst: "waiting_on_topic"},
+			{Name: "decline_trial", Src: []string{"awaiting_confirmation_of_trial"}, Dst: "inactive"},
+			{Name: "submit_topic", Src: []string{"waiting_on_topic"}, Dst: "waiting_on_items"},
 		},
-		fsm.Callbacks{
-            "enter_state": func(e *fsm.Event) { trial.enterState(e) },
-        },
+		fsm.Callbacks{},
 	)
 
 	logger := log.New(os.Stdout, "slack-bot:", log.Lshortfile|log.LstdFlags)
+	// need to set bot_handle to be dynamic as the id could change depending on the instance
 	bot_handle := "@U1QAH7PRD"
 	slack.SetLogger(logger)
 	var slack_token SlackToken
-	token_file, err  := getToken()
-	
+	token_file, err := getToken()
+
 	if err != nil {
 		fmt.Printf("File Error: %s\n", err)
 		os.Exit(1)
@@ -86,41 +110,40 @@ func main() {
 Loop:
 	for {
 		select {
-		case msg := <- rtm.IncomingEvents:
-				fmt.Print("Event Received: ")
-				switch ev := msg.Data.(type) {
-					case *slack.HelloEvent:
-						// Ignore
+		case msg := <-rtm.IncomingEvents:
+			fmt.Print("Event Received: ")
+			switch ev := msg.Data.(type) {
+			case *slack.HelloEvent:
+				// Ignore
 
-					case *slack.ConnectedEvent:
-						fmt.Println("Infos:", ev.Info)
-						fmt.Println("COnnection counter:", ev.ConnectionCount)
-						rtm.SendMessage(rtm.NewOutgoingMessage("Hello World", "#parliament-test"))
+			case *slack.ConnectedEvent:
+				fmt.Println("Infos:", ev.Info)
+				fmt.Println("COnnection counter:", ev.ConnectionCount)
+				rtm.SendMessage(rtm.NewOutgoingMessage("Hello World", "#parliament-test"))
 
-					case *slack.MessageEvent:
-						fmt.Printf("Message: %v\n", ev)
-						if strings.Contains(ev.Text, bot_handle) {
-							rtm.SendMessage(rtm.NewOutgoingMessage("Here ye, here ye! Should we hold a trial?", ev.Channel))							
-						}
-						fmt.Printf("Trial: %v\n", trial)
+			case *slack.MessageEvent:
+				fmt.Printf("Message: %v\n", ev)
+				if strings.Contains(ev.Text, bot_handle) {
+					HandleParliamentMessage(ev, rtm, trial)
+				}
+				fmt.Printf("Trial: %v\n", trial)
 
+			case *slack.PresenceChangeEvent:
+				fmt.Printf("Presence Change %v\n", ev)
 
-					case *slack.PresenceChangeEvent:
-						fmt.Printf("Presence Change %v\n", ev)
+			case *slack.LatencyReport:
+				fmt.Printf("Current latency: %v\n", ev.Value)
 
-					case *slack.LatencyReport:
-						fmt.Printf("Current latency: %v\n", ev.Value)
+			case *slack.RTMError:
+				fmt.Printf("Error: %s\n", ev.Error())
 
-					case *slack.RTMError:
-						fmt.Printf("Error: %s\n", ev.Error())
+			case *slack.InvalidAuthEvent:
+				fmt.Printf("Invalid credentials")
+				break Loop
 
-					case *slack.InvalidAuthEvent:
-						fmt.Printf("Invalid credentials")
-						break Loop
-
-					default:
-						// All events ignored
-				}	
+			default:
+				// All events ignored
+			}
 		}
 	}
 }
